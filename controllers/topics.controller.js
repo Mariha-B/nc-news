3dbd288936eaad10ba80e24bb5fd1f7bd53a3717
Mariha-B <mariha_butt@hotmail.com>
2024-02-19 15:47:36 +0000

    Refactored code to take any endpoint and added a 404 error check

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,8 +1,11 @@
 const { fetchTopics } = require("../models/topics.model");
 
 exports.getTopics = (req, res, next) => {
-  fetchTopics()
+    const { endpoint } = req.params;
+    console.log(endpoint);
+  fetchTopics(endpoint)
     .then((topics) => {
+        console.log(topics);
       res.status(200).send({topics});
     })
     .catch(next);
