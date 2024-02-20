cfb64a6b235fe4241fbcec55005a88d99d48341c
Mariha-B <mariha_butt@hotmail.com>
2024-02-20 12:26:07 +0000

    Task 4 Completed, incl tests, new endpoint to endpoint file, new psql error

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,8 @@ const request = require("supertest");
 const db = require("../db/connection.js");
 const seed = require("../db/seeds/seed.js");
 const data = require("../db/data/test-data");
-const endpoints = require("../endpoints.json")
+const endpoints = require("../endpoints.json");
+const utils = require("../db/seeds/utils.js");
 
 beforeEach(() => {
   return seed(data);
@@ -29,12 +30,53 @@ describe("/api/topics", () => {
     test("STATUS 404 - Responds with 404", () => {
       return request(app).get("/api/not-an-endpoint").expect(404);
     });
-    test("Responds with an object describing available endpoints on api", () => {
+  });
+});
+
+describe("/api", () => {
+  test("STATUS 200 - Responds with an object describing available endpoints on api", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body).toEqual(endpoints);
+      });
+  });
+});
+
+describe("/api/articles/:article_id", () => {
+  describe("GET", () => {
+    test("STATUS 200 - Responds with an article object of article_id", () => {
       return request(app)
-        .get("/api")
+        .get("/api/articles/1")
         .expect(200)
-        .then(({body}) => {
-          expect(body).toEqual(endpoints);
+        .then(({ body: { article } }) => {
+          expect(article.author).toBe("butter_bridge");
+          expect(article.title).toBe("Living in the shadow of a great man");
+          expect(article.article_id).toBe(1);
+          expect(article.body).toBe("I find this existence challenging");
+          expect(article.topic).toBe("mitch");
+          expect(article.created_at).toBe("2020-07-09T20:11:00.000Z");
+          expect(article.votes).toBe(100);
+          expect(article.article_img_url).toBe(
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+          );
+        });
+    });
+    test("STATUS 404 - Responds with not found when given a valid but non-existing id", () => {
+      return request(app)
+        .get("/api/articles/9999")
+        .expect(404)
+        .then(({ body: { msg } }) => {
+          expect(msg).toBe("article does not exist");
+        });
+    });
+    test("STATUS 400 - Responds with bad request when given an invalid id", () => {
+      return request(app)
+        .get("/api/articles/no-an-id")
+        .expect(400)
+        .then(({ body: { msg } }) => {
+          expect(msg).toBe("Bad request");
         });
     });
   });
