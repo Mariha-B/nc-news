cf8ddcc68cc00770038794e6b9acedd17aad735b
Mariha-B <mariha_butt@hotmail.com>
2024-02-22 10:12:34 +0000

    Task 6- added a test correction

--- app.js
+++ app.js
@@ -20,19 +20,24 @@ app.get("/api/articles/:article_id/comments", getComments);
 app.get("/api/articles", getArticles);
 
 //Error Handling
-//Custom Error(s)
-app.use((err, req, res, next) => {
-  if (err.status && err.msg) {
-    res.status(err.status).send({ msg: err.msg });
-  } else {
-    next(err);
-  }
-});
 
 //PSQL Errors
 app.use((err, req, res, next) => {
   if (err.code === "22P02" || err.code === "42703") {
     res.status(400).send({ msg: "Bad request" });
+  } else {
+    next(err);
+  }
+});
+
+app.use((req, res, next) => {
+  res.status(404).send({ msg: "Not Found" });
+});
+
+//Custom Error(s)
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
   } else {
     next(err);
   }
