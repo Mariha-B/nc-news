fed213f485c2d1debd73bb8de968cfb4dc6448ff
Mariha-B <mariha_butt@hotmail.com>
2024-02-23 13:23:54 +0000

    Host application Step 6

--- listener.js
+++ listener.js
@@ -1,13 +1,5 @@
 const app = require('./app.js');
 
-app.listen(8080, (error) =>
-{
-    if (error)
-    {
-        console.log(error);
-    }
-    else
-    {
-        console.log('Listening on port 8080!');
-    }
-});
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
