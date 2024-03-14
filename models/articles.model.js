d448bae50ffa1d3eee04168be833bb8b6366d24b
Mariha-B <mariha_butt@hotmail.com>
2024-03-14 15:43:03 +0000

    selectArticles modified so topics list is dynamic

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,5 +1,6 @@
 const db = require("../db/connection.js");
 const format = require("pg-format");
+const { fetchTopics } = require("./topics.model.js");
 
 exports.fetchArticle = (article_id) => {
   const queryString = `
@@ -18,7 +19,7 @@ exports.fetchArticle = (article_id) => {
     WHERE articles.article_id = $1
     GROUP BY articles.article_id;
   `;
-  
+
   return db.query(queryString, [article_id]).then(({ rows }) => {
     if (rows.length === 0) {
       return Promise.reject({ status: 404, msg: "article does not exist" });
@@ -34,39 +35,44 @@ exports.selectArticles = (
 ) => {
   // Greenlist
   const validOrders = ["ASC", "DESC"];
-  const validTopics = ["cat", "paper", "mitch", "coding", "football", "cooking", "%"];
   if (!validOrders.includes(order.toUpperCase())) {
     return Promise.reject({ status: 400, msg: "Bad Request" });
   }
-  if (!validTopics.includes(topic)) {
-    return Promise.reject({ status: 404, msg: "topic does not exist" });
-  }
-
-  const queryString = format(
-    `SELECT 
-      articles.author,
-      articles.title,
-      articles.article_id,
-      articles.topic,
-      articles.created_at,
-      articles.votes,
-      articles.article_img_url,
-    COUNT(comments.comment_id)::INT AS comment_count
-    FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id
-    WHERE articles.topic LIKE %L
-    GROUP BY articles.article_id
-    ORDER BY articles.%I %s;`,
-    topic,
-    sort_by,
-    order
-  );
-
-  return db.query(queryString).then(({ rows }) => {
-    if (rows.length === 0) {
-      return [];
-    }
-    return rows;
-  });
+  return fetchTopics()
+    .then((topics) => {
+      const foundTopic = topics.find((top) => top.slug === topic);
+      if (!foundTopic && topic !=='%') {
+        return Promise.reject({ status: 404, msg: "topic does not exist" });
+      }
+      const queryString = format(
+        `SELECT 
+        articles.author,
+        articles.title,
+        articles.article_id,
+        articles.topic,
+        articles.created_at,
+        articles.votes,
+        articles.article_img_url,
+      COUNT(comments.comment_id)::INT AS comment_count
+      FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id
+      WHERE articles.topic LIKE %L
+      GROUP BY articles.article_id
+      ORDER BY articles.%I %s;`,
+        topic,
+        sort_by,
+        order
+      );
+      return queryString;
+    })
+    .then((queryString) => {
+      return db.query(queryString);
+    })
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return [];
+      }
+      return rows;
+    });
 };
 
 exports.selectComments = (
